3c2ca01e7ca29cfb46023d6dabf0862a9f7bdea3
Vladislav Tonchuk <xodlava@gmail.com>
2017-12-22 13:34:41 +0400

    New Design

---
+++ app/assets/javascripts/options.js
@@ -0,0 +1,12 @@
+mr.typed.options = {
+  /**
+   * @property {boolean} loop loop strings
+   * @property {number} loopCount amount of loops
+   */
+  loop: false,
+  loopCount: Infinity,
+  /**
+   * @property {number} startDelay time before typing starts in milliseconds
+   */
+  startDelay: 1000,
+}
